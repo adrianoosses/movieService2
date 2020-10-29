6a42b00517c930d0db959df26053de9ab81e2ea7
aosses <adrianoosses9@gmail.com>
2020-10-29 13:32:52 +0100

    Nuevos modelos2

--- components/order/model.js
+++ components/order/model.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrderSchema = new moongose.Schema({
-    userId:{type: ObjectId}, //clave foranea, clave de otra tabla q en alli es primaria.
-    movieId:{type: ObjectId},
+    userId:{type: ObjectId, ref: 'User'}, //clave foranea, clave de otra tabla q en alli es primaria.
+    movieId:{type: ObjectId, ref: 'Movie'},
     createdAt: { type: Date, default: Date},
     refundDate: {type: Date}
 });
 
-new Order({userId: '489374893748',})
+//new Order({userId: '489374893748',})
+
